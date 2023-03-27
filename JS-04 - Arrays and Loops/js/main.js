83a42f167860a1b52e99bbbab96ce3894c8b274d
montoya <ing.montoyacom@gmail.com>
2023-03-27 16:55:30 -0600

    ya le agarre la onda a los .pop .shift

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,9 +1,17 @@
 const alerta = document.getElementById("Fila");
 
-let fila = ["Sofia","David","Juan"];
+let fila = [
+    "Sofia",
+    "David",
+    "Juan"
+];
 alerta.innerHTML = fila.length +" elementos<ul>"
 + "<ol><li>"+fila[0]+"</li>"
 + "<li>"+fila[1]+"</li>"
 + "<li>"+fila[2]+"</li>"
 console.log(fila); 
-console.log(fila.push("Sara"));
+fila.push("Sara","Agusto");
+console.log(fila); 
+fila.shift();
+console.log(fila);
+
